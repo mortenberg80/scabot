efba14ae331f8d860e4dad51c77bc06ad614fce1
Morten Berg <mortenberg80@gmail.com>
2011-03-08 22:33:51 +0100

    Fixed better output, changed server

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -1,11 +1,21 @@
 import java.net._
 import java.io._
 
+/*
+  HOST = 'irc.homelien.no'
+  PORT = 6667
+  NICK = 'mynick'
+  IDENT = 'pybot'
+  REALNAME = 'I am'
+  CHANNEL = '#mychannel'
+*/
+
 object Bot {
   def main(args: Array[String]) {
 
-    var hostname = "wineasy.se.quakenet.org"
-    var port = 6667
+    val hostname = "irc.no.quakenet.eu.org"
+    //val hostname = "irc.homelien.no"
+    val port = 6667
 
     var bot = new Bot(hostname, port)
     var botThread = new Thread(bot)
@@ -15,6 +25,7 @@ object Bot {
     var line = systemIn.readLine()
     while(line != "quit") {
       bot.sendMessage(line)
+      line = systemIn.readLine()
     }
     println("Hello, world!")
 
@@ -32,25 +43,26 @@ class Bot(var hostname:String, var port: Int) extends Runnable {
     echoSocket = new Socket(hostname, port)
     out = new PrintWriter(echoSocket.getOutputStream(), true)
     in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()))
-    out.println("NICK scabot")
-    out.println("USER scabot " + hostname + " bla scabot")
-    out.println("JOIN #oztest")
+
+    sendMessage("NICK scabot")
+    sendMessage("USER scabot " + hostname + " bla scabot")
+    sendMessage("JOIN #oztest")
   }
 
   def run() = {
     connect()
     
     var line = in.readLine()
-      while(line != null) {
+    while(line != null) {
       println(line)
       line = in.readLine()
       val tokens = line.split(" ")
-
     }
   }
 
   def sendMessage(message: String) {
-    out.write(message)
+    println("Writing message to server: " + message)
+    out.println(message)
   }
 
   def quit() = {
